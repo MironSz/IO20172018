7b71c88b6aacf70baf21f1e48e626f3138a115e5
Marek Smolarczyk <mareksmoalrczyk33@gmail.com>
2018-06-11 20:12:44 +0200

    Crude implementation of search view

--- kod/trendspotter/crawler/views.py
+++ kod/trendspotter/crawler/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from datetime import timedelta
 from django.utils import timezone
-from .models import WordQuery
+from .models import WordQuery, Word, WordOccurence
 import pytz
 
 
@@ -17,20 +17,58 @@ class HomeView(TemplateView):
         return self.render_to_response(context)
 
 
+# Number of days for which the word statistics are displayed 
+PAST_DAYS = 15
+
+
 class SearchView(LoginRequiredMixin, TemplateView):
     template_name = 'search.html'
 
+    # TODO: Add 'ignore polish diacritics' checkbox functionality
     def post(self, request, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         user = self.request.user
         now = timezone.now()
 
-        if request.method == 'POST':
+        result_occurrences = [(i, []) for i in range(0, PAST_DAYS)]
+        result_words = []
+        words_not_in_system = set()
+
+        if request.method == 'POST' and request.POST.get('search') is not None:
             words = request.POST.get('search').split(';')
+            for i in range(len(words)):
+                words[i] = words[i].strip()
+            # TODO: Remove equivalent words if case insensitive checkbox is on
+            words = set(words)  # removes duplicates, but does NOT preserve order
+            words.discard('')
+
             for word in words:
                 user_word, new = WordQuery.objects.get_or_create(client=user, time=now, word=word)
                 user_word.save()
 
+                if request.POST.get('case_ch'):
+                    word_obj = Word.objects.filter(word__iexact=word)
+                else:
+                    word_obj = Word.objects.filter(word=word)
+
+                if word_obj:
+                    occurrences = [0 for _ in range(PAST_DAYS)]
+                    word_occurrences = WordOccurence.objects.filter(word=word_obj[0])
+                    for occ in word_occurrences:
+                        comment = occ.comment
+                        time_delta = now - comment.time
+                        if 0 <= time_delta.days < PAST_DAYS:
+                            occurrences[time_delta.days] += 1
+
+                    result_words.append(word)
+                    for day in range(0, PAST_DAYS):
+                        result_occurrences[day][1].append(occurrences[day])
+                else:
+                    words_not_in_system.add(word)
+                
+        context['result_words'] = result_words
+        context['result_occurrences'] = result_occurrences
+        context['words_not_in_system'] = words_not_in_system
         return self.render_to_response(context)
 
     def get(self, request, *args, **kwargs):
