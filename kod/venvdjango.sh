e00a342e1255c9f976542558e6ee0dd76364af68
Pilusx <poteka@wp.pl>
2018-06-11 22:59:52 +0200

    Update venvdjango.sh

--- kod/venvdjango.sh
+++ kod/venvdjango.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-python3.5 -m venv py_django
+virtualenv --python=python3.5  py_django
+
 source py_django/bin/activate
 pip install --upgrade pip
 
 pip install -r requirements.txt
-
-#pip install django==2.0.4 xlrd ipython pylint mypy bs4
-#pip install scrapy scrapy-splash scrapyd scrapy-djangoitem
-#pip install social-auth-app-django
-#pip install django-utils
 deactivate
